7902734198e6ca5ed706323f76fbb9dcc3abf1fa
raventhecat333 <jdmyers2016@outlook.com>
2021-08-15 19:23:44 -0300

    tes

--- cogs/apis.py
+++ cogs/apis.py
@@ -7,12 +7,6 @@ import asyncio
 
 from discord.ext import commands
 
-# Only if you want to use variables that are in the config.json file.
-if not os.path.isfile("config.json"):
-    sys.exit("'config.json' not found! Please add it and try again.")
-else:
-    with open("config.json") as file:
-        config = json.load(file)
 
 
 # Here we name the cog and create a new class for the cog.
